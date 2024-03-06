64043bdf452d5385ea8760cd1a92dd3845872527
gsousa12 <wgsousa12@gmail.com>
2024-03-06 20:15:45 -0300

    Add Fist Commit

---
+++ PrimeiraApi/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿namespace PrimeiraApi.Models
+{
+    public class Pessoa
+    {
+        public Guid Id { get; set; }
+
+
+        public string Nome { get; set; }
+
+        
+        // Construtor
+        public Pessoa(Guid id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
---
+++ PrimeiraApi/Rotas/PessoaRotas.cs
@@ -0,0 +1,39 @@
+﻿using PrimeiraApi.Models;
+
+namespace PrimeiraApi.Rotas
+{
+    public static class PessoaRotas
+    {
+        public static List<Pessoa> Pessoas = new List<Pessoa>()
+        {
+            new Pessoa(Guid.NewGuid(), "Neymar"),
+            new Pessoa(Guid.NewGuid(), "Messi"),
+            new Pessoa(Guid.NewGuid(), "Cristiano"),
+        };
+        public static void MapPessoaRotas(this WebApplication app)
+        {
+            app.MapGet("/pessoas", () => Pessoas);
+
+            app.MapGet("/pessoas/{nome}", (string nome) => Pessoas.Find(x => x.Nome == nome));
+
+            app.MapPost("pessoas", (Pessoa pessoa) =>
+            {
+                Pessoas.Add(pessoa);
+                return Results.Ok(pessoa);
+            });
+
+            app.MapPut("/pessoas/{id:guid}", (Guid id,Pessoa pessoa) =>
+            {
+                var encontrado = Pessoas.Find(x => x.Id == id);
+
+                if(encontrado == null)
+                    return Results.NotFound();
+
+                encontrado.Nome = pessoa.Nome;
+
+                return Results.Ok(encontrado);
+            });
+            
+        }
+    }
+}
