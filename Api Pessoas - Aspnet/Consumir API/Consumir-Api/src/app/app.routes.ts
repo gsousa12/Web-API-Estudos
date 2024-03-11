62444714a086b7b0f209c77cfaf42e2cefda3d4f
gsousa12 <wgsousa12@gmail.com>
2024-03-11 13:42:22 -0300

    delete

--- Api Pessoas - Aspnet/Consumir API/Consumir-Api/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig } from '@angular/core'
-import {provideHttpClient} from '@angular/common/http'
-import { Routes } from '@angular/router';
-export const routes: Routes = [];
-
-export const appConfig: ApplicationConfig =  {
-    providers: [provideHttpClient()]
-};
